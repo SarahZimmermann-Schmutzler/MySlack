75e1ae25350785f0f819fc5a21c2c460be67b1a9
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-04-20 15:12:29 +0200

    animation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { WelcomeAnimationComponent } from './welcome-animation/welcome-animation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    WelcomeAnimationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/welcome-animation/welcome-animation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-animation',
+  templateUrl: './welcome-animation.component.html',
+  styleUrls: ['./welcome-animation.component.scss']
+})
+export class WelcomeAnimationComponent {
+
+}
