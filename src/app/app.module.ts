d400b1c0d636a2ad11b586c2bd9baa4e35e15524
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-04-21 08:48:20 +0200

    header login ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { WelcomeAnimationComponent } from './welcome-animation/welcome-animation.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    WelcomeAnimationComponent
+    WelcomeAnimationComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
