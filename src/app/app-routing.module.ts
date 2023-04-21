fe61b035fe73eebfbe3869c131de6212b994e392
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-04-21 15:09:57 +0200

    link first try

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { LoginProcessComponent } from './login-process/login-process.component';
 
 const routes: Routes = [
--- src/app/login-process/login-process.component.ts
+++ src/app/login-process/login-process.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-process',
   templateUrl: './login-process.component.html',
   styleUrls: ['./login-process.component.scss']
 })
-export class LoginProcessComponent {
+
+export class LoginProcessComponent implements OnInit{
+  login = true;
+  signUp = true;
+  password = true;
+
+  ngOnInit(): void {
+    this.login = false;
+  }
+
+  constructor () {
+  }
+
+  openSignUp() {
+    this.signUp = false;
+    this.login = true;
+  }
+
+  
+  
+
+  
+
 
 }
