01201e3cdcce6f580bccc26794baed5ec1e94560
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-04-28 14:08:28 +0200

    passwort zurücksetzen first

--- src/app/password/password.component.ts
+++ src/app/password/password.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Auth, sendPasswordResetEmail } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-password',
   templateUrl: './password.component.html',
   styleUrls: ['./password.component.scss']
 })
-export class PasswordComponent {
+export class PasswordComponent implements OnInit{
+  disabled = true;
+  popUp = false;
+  mail = '';
 
+  ngOnInit(): void {
+    setInterval(() => {
+      if(this.mail) {
+        this.disabled = false;
+      }
+    }, 1000);
+  }
+
+  constructor(private router: Router, private auth : Auth) {}
+
+  sendMail() {
+    sendPasswordResetEmail(this.auth, this.mail).then(() => {
+      this.clearFields();
+      this.disabled = true;
+      this.popUp = true;
+      // setTimeout(() => {
+      // this.router.navigate(['/']);
+      // }, 3000);
+    })
+  }
+
+  clearFields() {
+    this.mail = '';
+  }
 }
