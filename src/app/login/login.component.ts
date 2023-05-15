7493befb5f91ccfecd161104c1dfcc395c757a5c
SarahZimmermann-Schmutzler <114473989+SarahZimmermann-Schmutzler@users.noreply.github.com>
2023-05-15 10:19:15 +0200

    reload page first try

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Auth, onAuthStateChanged, signInWithEmailAndPassword } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 import { User } from 'src/models/user.class';
@@ -18,40 +18,62 @@ export class LoginComponent implements OnInit {
   @Input() currentUser = '';
   guestUser = 'kLLzHS4VI6TDTL2gZUPbRzgOoID3';
   coll = collection(this.firestore, 'users');
+  interval;
 
   ngOnInit(): void {
-    
+    // this.route.params.subscribe(() => {
+    //   console.log('Seite neu geladen');
+    //   this.interval = setInterval(() => {
+    //     window.location.reload()
+    //     this.stopReload();
+    //   }, 1000);
+
+    //   this.stopReload();
+    // }) 
+
+    // this.interval = setInterval(() => {
+    //   window.location.reload()
+    //   this.stopReload();
+    // }, 1000);
+
+    // this.stopReload();
   }
 
-  constructor (private router : Router, private auth : Auth, public firestore: Firestore) {}
+  constructor(private router: Router, private auth: Auth, public firestore: Firestore, private route: ActivatedRoute) {
+  
+  }
 
-  login() {  
-  signInWithEmailAndPassword(this.auth, this.mail, this.password).then(() => {
-        onAuthStateChanged(this.auth, (user$) => {
-          if(user$) {
-            this.currentUser = user$.uid;
-            // console.log(this.currentUser);
-            localStorage.setItem('currentUser', this.currentUser);
-            setDoc(doc(this.coll, this.currentUser), {status: 'Active'}, {merge: true});
-            // this.router.navigateByUrl('/workspace/'+ this.currentUser);
-            this.router.navigateByUrl('/workspace');
-          }
-        })
-      }, () => {
-        console.log('Fehlgeschlagen')
-        this.popUp = true;
-        setTimeout(() => {
-          this.mail = '';
-          this.password = '';
-          this.popUp = false;
-          }, 3000); 
-        })
-    }
+  stopReload() {
+    clearInterval(this.interval);
+  }
 
-    guestLogin() {
-      setDoc(doc(this.coll, this.guestUser), {name: 'Guest', mail: 'guest@guest.de', status: 'Active'});
-      localStorage.setItem('currentUser', this.guestUser);
-      // this.router.navigateByUrl('/workspace/'+ this.guestUser);
-      this.router.navigateByUrl('/workspace');
-    }
+  login() {
+    signInWithEmailAndPassword(this.auth, this.mail, this.password).then(() => {
+      onAuthStateChanged(this.auth, (user$) => {
+        if (user$) {
+          this.currentUser = user$.uid;
+          // console.log(this.currentUser);
+          localStorage.setItem('currentUser', this.currentUser);
+          setDoc(doc(this.coll, this.currentUser), { status: 'Active' }, { merge: true });
+          // this.router.navigateByUrl('/workspace/'+ this.currentUser);
+          this.router.navigateByUrl('/workspace');
+        }
+      })
+    }, () => {
+      console.log('Fehlgeschlagen')
+      this.popUp = true;
+      setTimeout(() => {
+        this.mail = '';
+        this.password = '';
+        this.popUp = false;
+      }, 3000);
+    })
+  }
+
+  guestLogin() {
+    setDoc(doc(this.coll, this.guestUser), { name: 'Guest', mail: 'guest@guest.de', status: 'Active' });
+    localStorage.setItem('currentUser', this.guestUser);
+    // this.router.navigateByUrl('/workspace/'+ this.guestUser);
+    this.router.navigateByUrl('/workspace');
+  }
 }
